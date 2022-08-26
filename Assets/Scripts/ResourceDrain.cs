f7884df6f6b312b71bee6eea76eac4a06eaa1d0f
AlexeyLobanov99 <111963462+AlexeyLobanov99@users.noreply.github.com>
2022-08-26 11:59:08 +0300

    Проект
    Загрузка проекта

---
+++ Assets/Scripts/ResourceDrain.cs
@@ -0,0 +1,1 @@
+using UnityEngine;public class ResourceDrain : MonoBehaviour{    public string resourceName;    public float drainPerSecond = 0.3f;    private PlayerResource resource;    void Start()    {        resource = PlayerResource.Find(resourceName);    }    void Update()    {        resource.ChangeValue(-drainPerSecond * Time.deltaTime);    }}
